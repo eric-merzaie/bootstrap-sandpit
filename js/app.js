ae1ddfba3387a63a27fef93b8cc6573188780c23
My Name <eric.merzaie@gmail.com>
2021-04-16 19:52:51 +1000

    added bounce animation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 // GSAP
 // gsap.from('body', {opacity:0, duration:1, y:50, ease: 'elastic(1, 0.3)'})
 
-var tl = gsap.timeline({defaults:{duration:1, opacity:0, ease:'elastic(1, 0.3)'}})
+const tl = gsap.timeline({defaults:{duration:1, opacity:0, ease:'elastic(1, 0.3)'}});
 
-tl.from("img, h1", {y:-200})
+tl.from("img, h1", {y:-200, ease:'bounce(1, 0.3)'})
     .from("#inputEmail", {x:100}, "-=.8")
     .from("#inputPassword", {x:-100}, "-=.8")
-    .from(".checkbox, .d-grid", {y:40}, "-=.8");
+    .from(".checkbox, .d-grid", {y:30}, "-=.8");
 
-document.getElementById("submit").addEventListener("click", () => {
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", () => {
+    tl.timeScale(1.5);
     tl.reversed() ? tl.play() : tl.reverse();
 })
